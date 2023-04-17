3b5fc50ab14a5eb761ad368d0adf63a0d195eae3
rahulbarik23 <rahul.barik1996@gmail.com>
2023-04-17 13:03:37 +0530

    change

---
+++ app.js
@@ -0,0 +1,17 @@
+function getUsers(callback) {
+    setTimeout(() => {
+      callback([
+        { username: 'john', email: 'john@test.com' },
+        { username: 'jane', email: 'jane@test.com' },
+      ]);
+    }, 1000);
+  }
+  
+  function findUser(username, callback) {
+    getUsers((users) => {
+      const user = users.find((user) => user.username === username);
+      callback(user);
+    });
+  }
+  
+  findUser('john', console.log);
